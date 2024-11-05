8acdded9e376a45d5acada9f417e4ad0b3c42784
brklntmhwk_w <86272619+brklntmhwk@users.noreply.github.com.>
2024-11-05 14:55:36 +0900

    fourth commit

--- src/utils.ts
+++ src/utils.ts
@@ -3,31 +3,31 @@ import type { ContainerDirective, LeafDirective } from "mdast-util-directive";
 import type { Node } from "unist";
 
 function isObject(target: unknown): target is { [k: string]: unknown } {
-  return typeof target === "object" && target !== null;
+	return typeof target === "object" && target !== null;
 }
 
 function isNode(node: unknown): node is Node {
-  return isObject(node) && "type" in node;
+	return isObject(node) && "type" in node;
 }
 
 function is<T extends Node>(node: unknown, type: string): node is T {
-  return isNode(node) && node.type === type;
+	return isNode(node) && node.type === type;
 }
 
 export function isParagraph(node: unknown): node is Paragraph {
-  return is(node, "paragraph");
+	return is(node, "paragraph");
 }
 
 export function isText(node: unknown): node is Text {
-  return is(node, "text");
+	return is(node, "text");
 }
 
 export function isLeafDirective(node: unknown): node is LeafDirective {
-  return is(node, "leafDirective");
+	return is(node, "leafDirective");
 }
 
 export function isContainerDirective(
-  node: unknown
+	node: unknown,
 ): node is ContainerDirective {
-  return is(node, "containerDirective");
+	return is(node, "containerDirective");
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -9,46 +9,46 @@ import { unified } from "unified";
 import remarkCard, { type Config } from "../src/index.js";
 
 const normalizeHtml = (html: string) => {
-  return html.replace(/[\n\s]*(<)|>([\n\s]*)/g, (_match, p1, _p2) =>
-    p1 ? "<" : ">"
-  );
+	return html.replace(/[\n\s]*(<)|>([\n\s]*)/g, (_match, p1, _p2) =>
+		p1 ? "<" : ">",
+	);
 };
 
 const parseMarkdown = mock(
-  async (markdown: string, options: Config, debug = false) => {
-    const remarkProcessor = unified()
-      .use(remarkParse)
-      .use(remarkDirective)
-      .use(remarkCard, options)
-      .use(remarkRehype)
-      .use(rehypeStringify);
+	async (markdown: string, options: Config, debug = false) => {
+		const remarkProcessor = unified()
+			.use(remarkParse)
+			.use(remarkDirective)
+			.use(remarkCard, options)
+			.use(remarkRehype)
+			.use(rehypeStringify);
 
-    if (debug) {
-      const remarkOutput = await remarkProcessor.run(
-        remarkProcessor.parse(markdown)
-      );
-      console.log("Remark output:", JSON.stringify(remarkOutput, null, 2));
-    }
+		if (debug) {
+			const remarkOutput = await remarkProcessor.run(
+				remarkProcessor.parse(markdown),
+			);
+			console.log("Remark output:", JSON.stringify(remarkOutput, null, 2));
+		}
 
-    const output = String(await remarkProcessor.process(markdown));
+		const output = String(await remarkProcessor.process(markdown));
 
-    if (debug) {
-      console.log(
-        `HTML output:
-      ${normalizeHtml(output)}`
-      );
-    }
+		if (debug) {
+			console.log(
+				`HTML output:
+      ${normalizeHtml(output)}`,
+			);
+		}
 
-    return output;
-  }
+		return output;
+	},
 );
 
 describe("Test the basic usage of video", () => {
-  const BASE_URL = "https://BASE_URL.com";
+	const BASE_URL = "https:\\BASE_URL.com";
 
-  test("Double-colon ver. of video", async () => {
-    const input = "::video{src=/videos/sample-video.mp4}";
-    const output = `
+	test("Double-colon ver. of video", async () => {
+		const input = "::video{src=/videos/sample-video.mp4}";
+		const output = `
     <div>
       <video controls preload="metadata">
         <source src="${BASE_URL}/videos/sample-video.mp4" type="video/mp4">
@@ -56,21 +56,21 @@ describe("Test the basic usage of video", () => {
     </div>
     `;
 
-    const html = await parseMarkdown(input, {
-      baseUrl: BASE_URL,
-      publicDir: "../public",
-    });
+		const html = await parseMarkdown(input, {
+			baseUrl: BASE_URL,
+			publicDir: "../public",
+		});
 
-    expect(normalizeHtml(html)).toBe(normalizeHtml(output));
-  });
+		expect(normalizeHtml(html)).toBe(normalizeHtml(output));
+	});
 
-  test("Triple-colon ver. of video", async () => {
-    const input = `
+	test("Triple-colon ver. of video", async () => {
+		const input = `
   :::video
   /videos/sample-video.mp4
   :::
     `;
-    const output = `
+		const output = `
     <div>
       <video controls preload="metadata">
         <source src="${BASE_URL}/videos/sample-video.mp4" type="video/mp4">
@@ -78,11 +78,11 @@ describe("Test the basic usage of video", () => {
     </div>
     `;
 
-    const html = await parseMarkdown(input, {
-      baseUrl: BASE_URL,
-      publicDir: "../public",
-    });
+		const html = await parseMarkdown(input, {
+			baseUrl: BASE_URL,
+			publicDir: "../public",
+		});
 
-    expect(normalizeHtml(html)).toBe(normalizeHtml(output));
-  });
+		expect(normalizeHtml(html)).toBe(normalizeHtml(output));
+	});
 });
