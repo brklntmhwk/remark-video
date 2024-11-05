4e9367ae930e5ea609e77872a62cff3631dbfd92
brklntmhwk_w <86272619+brklntmhwk@users.noreply.github.com.>
2024-11-05 14:51:42 +0900

    third commit

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,15 @@ import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "bun";
 import type { ElementContent } from "hast";
-import type { PhrasingContent, Root } from "mdast";
-import type { LeafDirective } from "mdast-util-directive";
+import type { Root } from "mdast";
 import type { Plugin } from "unified";
 import { visit } from "unist-util-visit";
-import {
-	isBareLink,
-	isContainerDirective,
-	isLeafDirective,
-	isParagraph,
-} from "./utils.js";
+import { isContainerDirective, isLeafDirective, isParagraph } from "./utils.js";
 
 export type Config = {
-	baseUrl: string;
-	publicDir: string;
-	videoContainerClass?: string;
+  baseUrl: string;
+  publicDir: string;
+  videoContainerClass?: string;
 };
 
 const __filename = fileURLToPath(import.meta.url);
@@ -27,126 +21,127 @@ const __dirname = path.dirname(__filename);
 const convertMp4ToWebM = (path: string) => `${path.slice(0, -3)}webm`;
 
 const remarkVideo: Plugin<[Config], Root> = (config) => {
-	const { baseUrl, publicDir, videoContainerClass } = config;
+  const { baseUrl, publicDir, videoContainerClass } = config;
 
-	return (tree) => {
-		visit(tree, isLeafDirective, (node) => {
-			if (node.name !== "video") return;
-			if (!node.attributes?.src || !node.attributes?.src.endsWith("mp4"))
-				return;
+  return (tree) => {
+    visit(tree, isLeafDirective, (node) => {
+      if (node.name !== "video") return;
+      if (!node.attributes?.src || !node.attributes?.src.endsWith("mp4"))
+        return;
 
-			const pathToMp4Video = path.join(
-				__dirname,
-				publicDir,
-				node.attributes.src,
-			);
-			const pathToWebMVideo = convertMp4ToWebM(pathToMp4Video);
-			const mp4VideoUrl = path.join(baseUrl, node.attributes.src);
+      const pathToMp4Video = path.join(
+        __dirname,
+        publicDir,
+        node.attributes.src
+      );
+      const pathToWebMVideo = convertMp4ToWebM(pathToMp4Video);
+      const mp4VideoUrl = path.join(baseUrl, node.attributes.src);
 
-			const sources: ElementContent[] = [];
+      const sources: ElementContent[] = [];
 
-			if (fs.existsSync(pathToWebMVideo)) {
-				sources.push({
-					type: "element",
-					tagName: "source",
-					properties: {
-						src: convertMp4ToWebM(mp4VideoUrl),
-						type: "video/webm",
-					},
-					children: [],
-				});
-			}
+      if (fs.existsSync(pathToWebMVideo)) {
+        sources.push({
+          type: "element",
+          tagName: "source",
+          properties: {
+            src: convertMp4ToWebM(mp4VideoUrl),
+            type: "video/webm",
+          },
+          children: [],
+        });
+      }
 
-			sources.push({
-				type: "element",
-				tagName: "source",
-				properties: {
-					src: mp4VideoUrl,
-					type: "video/mp4",
-				},
-				children: [],
-			});
+      sources.push({
+        type: "element",
+        tagName: "source",
+        properties: {
+          src: mp4VideoUrl,
+          type: "video/mp4",
+        },
+        children: [],
+      });
 
-			node.data = {
-				...node.data,
-				hName: "div",
-				hProperties: {
-					class: videoContainerClass || node.attributes?.class || undefined,
-				},
-				hChildren: [
-					{
-						type: "element",
-						tagName: "video",
-						properties: {
-							controls: true,
-							preload: "metadata",
-						},
-						children: sources,
-					},
-				],
-			};
-		});
+      node.data = {
+        ...node.data,
+        hName: "div",
+        hProperties: {
+          class: videoContainerClass || node.attributes?.class || undefined,
+        },
+        hChildren: [
+          {
+            type: "element",
+            tagName: "video",
+            properties: {
+              controls: true,
+              preload: "metadata",
+            },
+            children: sources,
+          },
+        ],
+      };
+    });
 
-		visit(tree, isContainerDirective, (node) => {
-			if (node.name !== "video") return;
-			if (node.children.length === 0) return;
+    visit(tree, isContainerDirective, (node) => {
+      if (node.name !== "video") return;
+      if (node.children.length === 0) return;
 
-			const firstNode = node.children[0];
-			if (!isParagraph(firstNode)) return;
-			if (firstNode.children.length === 0) return;
+      const firstNode = node.children[0];
+      if (!isParagraph(firstNode)) return;
+      if (firstNode.children.length === 0) return;
 
-			const url = firstNode.children[0];
-			if (url.type !== "text") return;
-			if (!url.value.endsWith("mp4")) return;
+      const url = firstNode.children[0];
+      if (url.type !== "text") return;
+      if (!url.value.endsWith("mp4")) return;
 
-			const pathToMp4Video = path.join(__dirname, publicDir, url.value);
-			const pathToWebMVideo = convertMp4ToWebM(pathToMp4Video);
+      const pathToMp4Video = path.join(__dirname, publicDir, url.value);
+      const pathToWebMVideo = convertMp4ToWebM(pathToMp4Video);
+      const mp4VideoUrl = path.join(baseUrl, url.value);
 
-			const sources: ElementContent[] = [];
+      const sources: ElementContent[] = [];
 
-			if (fs.existsSync(pathToWebMVideo)) {
-				sources.push({
-					type: "element",
-					tagName: "source",
-					properties: {
-						src: convertMp4ToWebM(url.value),
-						type: "video/webm",
-					},
-					children: [],
-				});
-			}
+      if (fs.existsSync(pathToWebMVideo)) {
+        sources.push({
+          type: "element",
+          tagName: "source",
+          properties: {
+            src: convertMp4ToWebM(mp4VideoUrl),
+            type: "video/webm",
+          },
+          children: [],
+        });
+      }
 
-			sources.push({
-				type: "element",
-				tagName: "source",
-				properties: {
-					src: url.value,
-					type: "video/mp4",
-				},
-				children: [],
-			});
+      sources.push({
+        type: "element",
+        tagName: "source",
+        properties: {
+          src: mp4VideoUrl,
+          type: "video/mp4",
+        },
+        children: [],
+      });
 
-			node.data = {
-				...node.data,
-				hName: "div",
-				hProperties: {
-					...node.attributes,
-					class: videoContainerClass || node.attributes?.class || undefined,
-				},
-				hChildren: [
-					{
-						type: "element",
-						tagName: "video",
-						properties: {
-							controls: true,
-							preload: "metadata",
-						},
-						children: sources,
-					},
-				],
-			};
-		});
-	};
+      node.data = {
+        ...node.data,
+        hName: "div",
+        hProperties: {
+          ...node.attributes,
+          class: videoContainerClass || node.attributes?.class || undefined,
+        },
+        hChildren: [
+          {
+            type: "element",
+            tagName: "video",
+            properties: {
+              controls: true,
+              preload: "metadata",
+            },
+            children: sources,
+          },
+        ],
+      };
+    });
+  };
 };
 
 export default remarkVideo;
--- test/index.test.ts
+++ test/index.test.ts
@@ -9,80 +9,80 @@ import { unified } from "unified";
 import remarkCard, { type Config } from "../src/index.js";
 
 const normalizeHtml = (html: string) => {
-	return html.replace(/[\n\s]*(<)|>([\n\s]*)/g, (_match, p1, _p2) =>
-		p1 ? "<" : ">",
-	);
+  return html.replace(/[\n\s]*(<)|>([\n\s]*)/g, (_match, p1, _p2) =>
+    p1 ? "<" : ">"
+  );
 };
 
 const parseMarkdown = mock(
-	async (markdown: string, options: Config, debug = false) => {
-		const remarkProcessor = unified()
-			.use(remarkParse)
-			.use(remarkDirective)
-			.use(remarkCard, options)
-			.use(remarkRehype)
-			.use(rehypeStringify);
+  async (markdown: string, options: Config, debug = false) => {
+    const remarkProcessor = unified()
+      .use(remarkParse)
+      .use(remarkDirective)
+      .use(remarkCard, options)
+      .use(remarkRehype)
+      .use(rehypeStringify);
 
-		if (debug) {
-			const remarkOutput = await remarkProcessor.run(
-				remarkProcessor.parse(markdown),
-			);
-			console.log("Remark output:", JSON.stringify(remarkOutput, null, 2));
-		}
+    if (debug) {
+      const remarkOutput = await remarkProcessor.run(
+        remarkProcessor.parse(markdown)
+      );
+      console.log("Remark output:", JSON.stringify(remarkOutput, null, 2));
+    }
 
-		const output = String(await remarkProcessor.process(markdown));
+    const output = String(await remarkProcessor.process(markdown));
 
-		if (debug) {
-			console.log(
-				`HTML output:
-      ${normalizeHtml(output)}`,
-			);
-		}
+    if (debug) {
+      console.log(
+        `HTML output:
+      ${normalizeHtml(output)}`
+      );
+    }
 
-		return output;
-	},
+    return output;
+  }
 );
 
 describe("Test the basic usage of video", () => {
-	const BASE_URL = "https:\\BASE_URL.com";
+  const BASE_URL = "https://BASE_URL.com";
 
-	test("Double-colon ver. of video", async () => {
-		const input = "::video{src=/videos/sample-video.mp4}";
-		const output = `
+  test("Double-colon ver. of video", async () => {
+    const input = "::video{src=/videos/sample-video.mp4}";
+    const output = `
     <div>
       <video controls preload="metadata">
-        <source src="${BASE_URL}\\videos\\sample-video.mp4" type="video/mp4">
+        <source src="${BASE_URL}/videos/sample-video.mp4" type="video/mp4">
       </video>
     </div>
     `;
 
-		const html = await parseMarkdown(input, {
-			baseUrl: BASE_URL,
-			publicDir: "../public",
-		});
+    const html = await parseMarkdown(input, {
+      baseUrl: BASE_URL,
+      publicDir: "../public",
+    });
 
-		expect(normalizeHtml(html)).toBe(normalizeHtml(output));
-	});
+    expect(normalizeHtml(html)).toBe(normalizeHtml(output));
+  });
 
-	test("Triple-colon ver. of video", async () => {
-		const input = `
+  test("Triple-colon ver. of video", async () => {
+    const input = `
   :::video
-  ${BASE_URL}\\videos\\sample-video.mp4
+  /videos/sample-video.mp4
   :::
     `;
-		const output = `
+    const output = `
     <div>
       <video controls preload="metadata">
-        <source src="${BASE_URL}\\videos\\sample-video.mp4" type="video/mp4">
+        <source src="${BASE_URL}/videos/sample-video.mp4" type="video/mp4">
       </video>
     </div>
     `;
 
-		const html = await parseMarkdown(input, {
-			baseUrl: BASE_URL,
-			publicDir: "../public",
-		});
+    const html = await parseMarkdown(input, {
+      baseUrl: BASE_URL,
+      publicDir: "../public",
+    });
 
-		expect(normalizeHtml(html)).toBe(normalizeHtml(output));
-	});
+    expect(normalizeHtml(html)).toBe(normalizeHtml(output));
+  });
 });
