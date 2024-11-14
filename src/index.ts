4a9881ce698eb84818e754042c6bcb87940b6cf6
brklntmhwk_w <86272619+brklntmhwk@users.noreply.github.com.>
2024-11-14 13:15:46 +0900

    refactor: make the baseUrl and publicDir optional

--- src/index.ts
+++ src/index.ts
@@ -8,214 +8,214 @@ import type { PhrasingContent, Root } from "mdast";
 import type { Plugin } from "unified";
 import { visit } from "unist-util-visit";
 import {
-	isContainerDirective,
-	isLeafDirective,
-	isParagraph,
-	isText,
+  isContainerDirective,
+  isLeafDirective,
+  isParagraph,
+  isText,
 } from "./utils.js";
 
 export type Config = {
-	baseUrl: string;
-	publicDir: string;
-	videoContainerTag?: string;
-	videoContainerClass?: string;
-	fallbackContent?: Readonly<ElementContent> | null | undefined;
+  baseUrl?: string;
+  publicDir?: string;
+  videoContainerTag?: string;
+  videoContainerClass?: string;
+  fallbackContent?: Readonly<ElementContent> | null | undefined;
 };
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const convertMp4To = (path: string, format: string) =>
-	`${path.slice(0, -3)}${format}`;
+  `${path.slice(0, -3)}${format}`;
 
 const remarkVideo: Plugin<[Config], Root> = (config) => {
-	const {
-		baseUrl,
-		publicDir,
-		videoContainerTag = "div",
-		videoContainerClass,
-		fallbackContent,
-	} = config;
+  const {
+    baseUrl = "",
+    publicDir = "./public",
+    videoContainerTag = "div",
+    videoContainerClass,
+    fallbackContent,
+  } = config;
 
-	return (tree) => {
-		visit(tree, isLeafDirective, (node) => {
-			if (node.name !== "video") return;
-			if (!node.attributes?.src || !node.attributes?.src.endsWith("mp4"))
-				return;
+  return (tree) => {
+    visit(tree, isLeafDirective, (node) => {
+      if (node.name !== "video") return;
+      if (!node.attributes?.src || !node.attributes?.src.endsWith("mp4"))
+        return;
 
-			let title: string | undefined = undefined;
+      let title: string | undefined = undefined;
 
-			if (node.children.length > 0 && isText(node.children[0])) {
-				title = node.children[0].value;
-			}
+      if (node.children.length > 0 && isText(node.children[0])) {
+        title = node.children[0].value;
+      }
 
-			const pathToMp4Video = path.join(
-				__dirname,
-				publicDir,
-				node.attributes.src,
-			);
-			const pathToWebMVideo = convertMp4To(pathToMp4Video, "webm");
-			const pathToOggVideo = convertMp4To(pathToMp4Video, "ogg");
-			const mp4VideoUrl = path.join(baseUrl, node.attributes.src);
+      const pathToMp4Video = path.join(
+        __dirname,
+        publicDir,
+        node.attributes.src
+      );
+      const pathToWebMVideo = convertMp4To(pathToMp4Video, "webm");
+      const pathToOggVideo = convertMp4To(pathToMp4Video, "ogg");
+      const mp4VideoUrl = path.join(baseUrl, node.attributes.src);
 
-			const sources: ElementContent[] = [];
+      const sources: ElementContent[] = [];
 
-			if (fs.existsSync(pathToWebMVideo)) {
-				sources.push({
-					type: "element",
-					tagName: "source",
-					properties: {
-						src: convertMp4To(mp4VideoUrl, "webm"),
-						type: "video/webm",
-					},
-					children: [],
-				});
-			}
+      if (fs.existsSync(pathToWebMVideo)) {
+        sources.push({
+          type: "element",
+          tagName: "source",
+          properties: {
+            src: convertMp4To(mp4VideoUrl, "webm"),
+            type: "video/webm",
+          },
+          children: [],
+        });
+      }
 
-			if (fs.existsSync(pathToOggVideo)) {
-				sources.push({
-					type: "element",
-					tagName: "source",
-					properties: {
-						src: convertMp4To(mp4VideoUrl, "ogg"),
-						type: "video/ogg",
-					},
-					children: [],
-				});
-			}
+      if (fs.existsSync(pathToOggVideo)) {
+        sources.push({
+          type: "element",
+          tagName: "source",
+          properties: {
+            src: convertMp4To(mp4VideoUrl, "ogg"),
+            type: "video/ogg",
+          },
+          children: [],
+        });
+      }
 
-			sources.push({
-				type: "element",
-				tagName: "source",
-				properties: {
-					src: mp4VideoUrl,
-					type: "video/mp4",
-				},
-				children: [],
-			});
+      sources.push({
+        type: "element",
+        tagName: "source",
+        properties: {
+          src: mp4VideoUrl,
+          type: "video/mp4",
+        },
+        children: [],
+      });
 
-			if (fallbackContent) {
-				sources.push(fallbackContent);
-			}
+      if (fallbackContent) {
+        sources.push(fallbackContent);
+      }
 
-			node.data = {
-				...node.data,
-				hName: videoContainerTag,
-				hProperties: {
-					class: videoContainerClass || node.attributes?.class || undefined,
-					title: title,
-					dataRemarkVideoFigure: true,
-				},
-				hChildren: [
-					{
-						type: "element",
-						tagName: "video",
-						properties: {
-							controls: true,
-							preload: "metadata",
-							width: "100%",
-						},
-						children: sources,
-					},
-				],
-			};
-		});
+      node.data = {
+        ...node.data,
+        hName: videoContainerTag,
+        hProperties: {
+          class: videoContainerClass || node.attributes?.class || undefined,
+          title: title,
+          dataRemarkVideoFigure: true,
+        },
+        hChildren: [
+          {
+            type: "element",
+            tagName: "video",
+            properties: {
+              controls: true,
+              preload: "metadata",
+              width: "100%",
+            },
+            children: sources,
+          },
+        ],
+      };
+    });
 
-		visit(tree, isContainerDirective, (node) => {
-			if (node.name !== "video") return;
-			if (node.children.length === 0) return;
+    visit(tree, isContainerDirective, (node) => {
+      if (node.name !== "video") return;
+      if (node.children.length === 0) return;
 
-			const [firstNode, secondNode, ..._restNodes] = node.children;
-			if (!isParagraph(firstNode)) return;
-			if (firstNode.children.length === 0) return;
+      const [firstNode, secondNode, ..._restNodes] = node.children;
+      if (!isParagraph(firstNode)) return;
+      if (firstNode.children.length === 0) return;
 
-			let title: string | undefined = undefined;
-			let url: PhrasingContent;
+      let title: string | undefined = undefined;
+      let url: PhrasingContent;
 
-			if (firstNode.data?.directiveLabel === true) {
-				if (firstNode.children.length > 0 && isText(firstNode.children[0])) {
-					title = firstNode.children[0].value;
-				}
+      if (firstNode.data?.directiveLabel === true) {
+        if (firstNode.children.length > 0 && isText(firstNode.children[0])) {
+          title = firstNode.children[0].value;
+        }
 
-				if (!isParagraph(secondNode)) return;
-				if (secondNode.children.length === 0) return;
-				url = secondNode.children[0];
-			} else {
-				url = firstNode.children[0];
-			}
+        if (!isParagraph(secondNode)) return;
+        if (secondNode.children.length === 0) return;
+        url = secondNode.children[0];
+      } else {
+        url = firstNode.children[0];
+      }
 
-			if (!isText(url)) return;
-			if (!url.value.endsWith("mp4")) return;
+      if (!isText(url)) return;
+      if (!url.value.endsWith("mp4")) return;
 
-			const pathToMp4Video = path.join(__dirname, publicDir, url.value);
-			const pathToWebMVideo = convertMp4To(pathToMp4Video, "webm");
-			const pathToOggVideo = convertMp4To(pathToMp4Video, "ogg");
-			const mp4VideoUrl = path.join(baseUrl, url.value);
+      const pathToMp4Video = path.join(__dirname, publicDir, url.value);
+      const pathToWebMVideo = convertMp4To(pathToMp4Video, "webm");
+      const pathToOggVideo = convertMp4To(pathToMp4Video, "ogg");
+      const mp4VideoUrl = path.join(baseUrl, url.value);
 
-			const sources: ElementContent[] = [];
+      const sources: ElementContent[] = [];
 
-			if (fs.existsSync(pathToWebMVideo)) {
-				sources.push({
-					type: "element",
-					tagName: "source",
-					properties: {
-						src: convertMp4To(mp4VideoUrl, "webm"),
-						type: "video/webm",
-					},
-					children: [],
-				});
-			}
+      if (fs.existsSync(pathToWebMVideo)) {
+        sources.push({
+          type: "element",
+          tagName: "source",
+          properties: {
+            src: convertMp4To(mp4VideoUrl, "webm"),
+            type: "video/webm",
+          },
+          children: [],
+        });
+      }
 
-			if (fs.existsSync(pathToOggVideo)) {
-				sources.push({
-					type: "element",
-					tagName: "source",
-					properties: {
-						src: convertMp4To(mp4VideoUrl, "ogg"),
-						type: "video/ogg",
-					},
-					children: [],
-				});
-			}
+      if (fs.existsSync(pathToOggVideo)) {
+        sources.push({
+          type: "element",
+          tagName: "source",
+          properties: {
+            src: convertMp4To(mp4VideoUrl, "ogg"),
+            type: "video/ogg",
+          },
+          children: [],
+        });
+      }
 
-			sources.push({
-				type: "element",
-				tagName: "source",
-				properties: {
-					src: mp4VideoUrl,
-					type: "video/mp4",
-				},
-				children: [],
-			});
+      sources.push({
+        type: "element",
+        tagName: "source",
+        properties: {
+          src: mp4VideoUrl,
+          type: "video/mp4",
+        },
+        children: [],
+      });
 
-			if (fallbackContent) {
-				sources.push(fallbackContent);
-			}
+      if (fallbackContent) {
+        sources.push(fallbackContent);
+      }
 
-			node.data = {
-				...node.data,
-				hName: videoContainerTag,
-				hProperties: {
-					...node.attributes,
-					class: videoContainerClass || node.attributes?.class || undefined,
-					title: title,
-					dataRemarkVideoFigure: true,
-				},
-				hChildren: [
-					{
-						type: "element",
-						tagName: "video",
-						properties: {
-							controls: true,
-							preload: "metadata",
-							width: "100%",
-						},
-						children: sources,
-					},
-				],
-			};
-		});
-	};
+      node.data = {
+        ...node.data,
+        hName: videoContainerTag,
+        hProperties: {
+          ...node.attributes,
+          class: videoContainerClass || node.attributes?.class || undefined,
+          title: title,
+          dataRemarkVideoFigure: true,
+        },
+        hChildren: [
+          {
+            type: "element",
+            tagName: "video",
+            properties: {
+              controls: true,
+              preload: "metadata",
+              width: "100%",
+            },
+            children: sources,
+          },
+        ],
+      };
+    });
+  };
 };
 
 export default remarkVideo;
